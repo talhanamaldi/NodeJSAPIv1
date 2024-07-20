263d57f8f3f2ff39704d97fb6f821dda7da0202c
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2024-07-20 14:18:17 +0300

    Update employeeController.js

--- app/controllers/employeeController.js
+++ app/controllers/employeeController.js
@@ -2,30 +2,6 @@ const db = require("../models");
 const Employee = db.employees;
 const Op = db.Sequelize.Op;
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Book:
- *       type: object
- *       required:
- *         - title
- *         - author
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the book
- *         title:
- *           type: string
- *           description: The book title
- *         author:
- *           type: string
- *           description: The book author
- *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- */
 
 exports.create = (req, res) => {
   if (!req.body.name) {
